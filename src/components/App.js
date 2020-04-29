57e54f479a4e7503bc26846b41074f549c302989
JKaram <jamiekaram16@gmail.com>
2020-04-29 15:09:34 -0400

    Add global style

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Button } from 'components/common'
 
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #fff;
+    min-height: 100vh;
+    margin: 0;
+    color: #000;
+    font-family: 'Kaushan Script'
+  }
+`
+
 
 function App() {
   return (
-    <h1>
-      Yoyo
+    <>
+      <GlobalStyle />
+      <h1>Yoyo</h1>
       <Button>TEST</Button>
-    </h1>
+
+    </>
   );
 }
 
